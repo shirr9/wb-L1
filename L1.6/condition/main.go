f95e152e1b10c7e9d0cd986253bdc92b21cfb19a
shirr9 <simkagaechka1@gmail.com>
2025-10-25 21:20:53 +0300

    feature: add goroutine exit by condition

---
+++ L1.6/condition/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+func main() {
+	var done atomic.Bool
+	c := make(chan int)
+	go func() {
+		cnt := 0
+		for v := range c {
+			if cnt < 10 {
+				fmt.Println(v)
+			}
+			cnt++
+			if cnt == 10 {
+				done.Store(true)
+			}
+		}
+	}()
+
+	for i := 0; i < 100; i++ {
+		if done.Load() {
+			break
+		}
+		c <- i
+	}
+	close(c)
+}
